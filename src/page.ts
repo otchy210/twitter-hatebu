764a956bc799a20c4aebf1c11423e3a7a70fd4f1
Otchy <id+github@otchy.net>
2023-04-10 23:56:28 +0900

    Add an icon in the entry page

--- src/page.ts
+++ src/page.ts
@@ -1,1 +1,35 @@
-console.log('page.ts');
+const iconPng = chrome.runtime.getURL('images/icon128.png');
+
+const handleContainer = (container: HTMLDivElement) => {
+    const menu = container.querySelector('.js-bookmark-menu');
+    const li = document.createElement('li');
+    li.classList.add('entry-myBookmark-tweet');
+    li.innerHTML = `
+        <button type="button" class="entry-myBookmark-menu-btn">
+            <span class="entry-myBookmark-popup">Twitter を開く</span>
+        </button>`;
+    menu.insertBefore(li, menu.firstChild);
+};
+
+const observer = new MutationObserver((records: MutationRecord[]) => {
+    records
+        .filter((record) => {
+            const target = record.target as HTMLDivElement;
+            return target.classList.contains('js-entry-myBookmark-container');
+        })
+        .map((record) => {
+            return record.target;
+        })
+        .forEach((target) => {
+            handleContainer(target as HTMLDivElement);
+        });
+});
+observer.observe(document.body, { childList: true, subtree: true });
+
+const style = document.createElement('style');
+style.innerHTML = `
+    .entry-myBookmark-tweet button:before {
+        -webkit-mask-image: url(/953af105e6ecbf610a0325354f889d29acbbb0b7/images/v4/public/icons/ic-arrow-turn-up-right.svg);
+    }
+`;
+document.head.appendChild(style);
